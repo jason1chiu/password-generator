5e077852ea43b7fee0d1552626e8766a74874ef3
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-01-04 16:14:08 -0500

    change switch to function

--- script.js
+++ script.js
@@ -8,11 +8,6 @@ function writePassword() {
 
     function generatePassword() {
 
-        // variables for different types of characters 
-        var lowercase = "abcdefghijklmnopqrstuvwxyz";
-        var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var number = "0123456789";
-        var special = "!:#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
         // prompting for an answer to indicate how many characters does the user want in their password
         var passwordLength = prompt("Choose the number of characters (8 to 128) in your password.");
@@ -21,7 +16,7 @@ function writePassword() {
             return "Thank you for trying my password generator!"
         }
         // while loop for passwordLength until conditions are met
-        while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+        while (!(passwordLength >= 8 || passwordLength <= 128) && isNaN(passwordLength)) {
             // alerts and prompts again if conditionals are not met
             alert("Must be a number between 8 and 128");
             passwordLength = prompt("Choose the number of characters (8 to 128) in your password.");
@@ -30,7 +25,7 @@ function writePassword() {
                 return "Thank you for trying my password generator!"
             }
             // breaks out of while loop to move on to next prompt
-            if (passwordLength >= 8 && passwordLength <= 128) {
+            if (passwordLength >= 8 || passwordLength <= 128) {
                 break;
             }
         }
@@ -99,83 +94,42 @@ function writePassword() {
             }
         }
 
-        // generates a string for the answers given in the prompts
         var response = lowercaseChars + uppercaseChars + numericChars + specialChars;
 
         // generates an empty string to highlight different situations
-        var charsSet = ""
+        var charsSet = "";
 
-        // switch among the 15 possible cases provided by response
-        switch (response) {
-            case "YYYY":
-                charsSet = lowercase + uppercase + number + special;
-                break;
-            case "YYYN":
-                charsSet = lowercase + uppercase + number;
-                break;
-            case "YYNY":
-                charsSet = lowercase + uppercase + special;
-                break;
-            case "YNYY":
-                charsSet = lowercase + number + special;
-                break;
-            case "YYNN":
-                charsSet = lowercase + uppercase;
-                break;
-            case "YNYN":
-                charsSet = lowercase + number;
-                break;
-            case "YNNY":
-                charsSet = lowercase + special;
-                break;
-            case "YNNN":
-                charsSet = lowercase;
-                break;
-            case "NYYY":
-                charsSet = uppercase + number + special;
-                break;
-            case "NYYN":
-                charsSet = uppercase + number;
-                break;
-            case "NYNY":
-                charsSet = uppercase + special;
-                break;
-            case "NYNN":
-                charsSet = uppercase;
-                break;
-            case "NNYY":
-                charsSet = number + special;
-                break;
-            case "NNYN":
-                charsSet = number;
-                break;
-            case "NNNY":
-                charsSet = special;
-                break;
+        function findCharsSet(type) {
+
+            // variables for different types of characters 
+            var lowercase = "abcdefghijklmnopqrstuvwxyz";
+            var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var number = "0123456789";
+            var special = "!:#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+            // invalid length of response
+            if (type.length !== 4) {
+                alert("invalid length of response argument");
+                return;
+            }
+
+            if (type[0] === "Y") { charsSet += lowercase };
+            if (type[1] === "Y") { charsSet += uppercase };
+            if (type[2] === "Y") { charsSet += number};
+            if (type[3] === "Y") { charsSet += special};
         }
 
+        findCharsSet(response);
+            
         var array = new Uint32Array(charsSet.length);
 
         window.crypto.getRandomValues(array);
-        var result = ''
-
+        var result = ""
+    
         for (var i = 0; i < passwordLength; i++) {
             result += charsSet[array[i] % charsSet.length];
         }
         return result;
-
-        /*       // generates an empty string to output a password
-              var result = "";
-
-              // for loop for the password generation at consecutive indices
-              for (var i = 0; i < passwordLength; i++) {
-                // creates an random number between 0 and 1, multiply it by the length of charsSet, and rounding it down to the nearest integer
-                var randomNumber = Math.floor(Math.random() * charsSet.length);
-                // generates a string by adding characters consecutively
-                result += charsSet.charAt(randomNumber);
-              }
-              // returns the password
-              return result; */
     }
 
     passwordText.value = password;
