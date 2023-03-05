7c7ed93d90b37c2847d85325513ee80af6b5bd86
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-03-05 17:21:38 -0500

    update prompts and alerts to modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,164 +1,155 @@
-// Assignment Code
+// This variable selects the button element with the ID "generate"
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+// This creates a new instance of the Bootstrap Modal for notifications
+const notifyModal = new bootstrap.Modal(document.getElementById('notifyModal'), {})
 
-    // a function to generate a password
-    function generatePassword() {
+// This function writes the generated password to the password field in the HTML document
+async function writePassword() {
 
-        var passwordLength = prompt("Choose the number of characters (8 to 128) in your password.");    // prompting for an answer to indicate how many characters does the user want in their password
-        
-        if (passwordLength === "" || passwordLength === null) {
-            return "Thank you for trying my password generator!"                                        // returns a message if user decides to cancel or enter no response
-        }
+  // This variable awaits the generated password from the generatePassword() function
+  var password = await generatePassword();
 
-        // while loop for passwordLength until conditions are met
-        while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-            
-            alert("Must be a number between 8 and 128");                                                // alert the user that their input is invalid
-            passwordLength = prompt("Choose the number of characters (8 to 128) in your password.");    // ask the question again
-            
-            if (passwordLength === "" || passwordLength === null) {
-                return "Thank you for trying my password generator!"                                    // returns a message if user decides to cancel or enter no response
-            }
+  // This variable selects the HTML element where the generated password will be written
+  var passwordText = document.querySelector("#password");
 
-            if (passwordLength >= 8 && passwordLength <= 128) {
-                break;                                                                                  // breaks the while loop so user can answer the next question if previous answer is valid
-            }
-        }
-
-        // prompting for an answer to include lowercase characters in password
-        var lowercaseChars = prompt("Do you want to include lowercase characters in your password? [Y for yes or N for no]");
-        // returns a message if user decides to cancel or enter no response
-        if (lowercaseChars === "" || lowercaseChars === null) {
-            return "Thank you for trying my password generator!"
-        }
-        // while loop for lowercaseChars until conditions are met
-        while (!(lowercaseChars === "Y" || lowercaseChars === "N")) {
-            // alerts and prompts again if conditionals are not met
-            alert("Must be Y or N");
-            lowercaseChars = prompt("Do you want to include lowercase characters in your password? [Y for yes or N for no]");
-            // returns a message if user decides to cancel or enter no response
-            if (lowercaseChars === "" || lowercaseChars === null) {
-                return "Thank you for trying my password generator!"
-            }
-            // breaks out of while loop to move on to next prompt
-            if (lowercaseChars === "Y" || lowercaseChars === "N") {
-                break;
-            }
-        }
-
-        // prompting for an answer to include upperase characters in password
-         var uppercaseChars = prompt("Do you want to include uppercase characters in your password? [Y for yes or N for no]");
-         if (uppercaseChars === "" || uppercaseChars === null) {
-             return "Thank you for trying my password generator!"
-         }
-         // while loop for uppercaseChars until conditions are met
-         while (!(uppercaseChars === "Y" || uppercaseChars === "N")) {
-             // alerts and prompts again if conditionals are not met
-             alert("Must be Y or N");
-             uppercaseChars = prompt("Do you want to include uppercase characters in your password? [Y for yes or N for no]");
-             if (uppercaseChars === "" || uppercaseChars === null) {
-                 return "Thank you for trying my password generator!"
-             }
-             // breaks out of while loop to move on to next prompt
-             if (uppercaseChars === "Y" || uppercaseChars === "N") {
-                 break;
-             }
-         }
-
-          // prompting for an answer to include numeric characters in password
-         var numericChars = prompt("Do you want to include numeric characters in your password? [Y for yes or N for no]");
-         // while loop for numericChars until conditions are met
-         while (!(numericChars === "Y" || numericChars === "N")) {
-             alert("Must be Y or N");
-             numericChars = prompt("Do you want to include numeric characters in your password? [Y for yes or N for no]");
-             // breaks out of while loop to move on to next prompt
-             if (numericChars === "Y" || numericChars === "N") {
-                 break;
-             }
-         }
-
-        // prompting for an answer to include special characters in password
-        var specialChars = prompt("Do you want to include special characters in your password? [Y for yes or N for no]");
-         // while loop for specialChars until conditions are met
-         while (!(specialChars === "Y" || specialChars === "N")) {
-             alert("Must be Y or N");
-             specialChars = prompt("Do you want to include special characters in your password? [Y for yes or N for no]");
-             // breaks out of while loop to move on to next step
-             if (specialChars === "Y" || specialChars === "N") {
-                 break;
-             }
-            }
-        
-        var response = lowercaseChars + uppercaseChars + numericChars + specialChars;
-
-        // generates an empty string to highlight different situations
-        var charsSet = "";
-
-        // a function to add to the empty string charsSet with the types of characters
-        function findCharsSet(type) {
-
-            // variables for different types of characters 
-            var lowercase = "abcdefghijklmnopqrstuvwxyz";
-            var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var number = "0123456789";
-            var special = "!:#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-            // invalid length of response
-            if (type.length !== 4) {
-                alert("invalid length of response argument");
-                return;
-            }
-
-            // conditionals for including which characters to have
-            if (type[0] === "Y") { charsSet += lowercase };
-            if (type[1] === "Y") { charsSet += uppercase };
-            if (type[2] === "Y") { charsSet += number };
-            if (type[3] === "Y") { charsSet += special };
-
-            if (type[0] === "N" && type[1] === "N" && type[2] === "N" && type[3] === "N") {
-                alert("invalid response to create password");
-            }
-        }
-
-        // function to find the characters based on response
-        findCharsSet(response);
-        
-        // generates an empty string to fill in password
-        var result = ""
-
-        // for loop for the password generation at consecutive indices
-        for (var i = 0; i < passwordLength; i++) {
-            // creates an random number between 0 and 1, multiply it by the length of charsSet, and rounding it down to the nearest integer
-            var randomNumber = Math.floor(Math.random() * charsSet.length);
-            // generates a string by adding characters consecutively
-            result += charsSet.charAt(randomNumber);
-        }
-        // returns the password
-        return result;
-
-        /* // create an empty array of unsigned 32-bit integers based of the number of characters in charsSet 
-        var array = new Uint32Array(charsSet.length);
-
-        // fills the array with random numbers in its cryptographic meaning
-        window.crypto.getRandomValues(array);
-
-        // generates an empty string to fill in password
-        var result = ""
+  // This function generates the password with user inputs and returns it
+  async function generatePassword() {
     
-        // generates the password one character at a time
-        for (var i = 0; i < passwordLength; i++) {
-            result += charsSet[array[i] % charsSet.length];
-        }
-        return result; */
+    // These variables await user input from the custom prompt function myPrompt()
+    var passwordLength = await myPrompt("Choose the number of characters (8 to 128) in your password.", 'number');
+    var lowercaseChars = await myPrompt("Do you want to include lowercase characters in your password? [Y for yes or N for no]", 'boolean');
+    var uppercaseChars = await myPrompt("Do you want to include uppercase characters in your password? [Y for yes or N for no]",'boolean');
+    var numericChars = await myPrompt("Do you want to include numeric characters in your password? [Y for yes or N for no]", 'boolean');
+    var specialChars = await myPrompt("Do you want to include special characters in your password? [Y for yes or N for no]",'boolean');
+
+    // This condition checks if all user responses are "N" and shows an error message if they are
+    if (lowercaseChars === "N" && uppercaseChars === "N" && numericChars === "N" && specialChars === "N") {
+      myAlert("Your responses cannot all be 'No'");
     }
 
-    passwordText.value = password;
+    // This hides the notification modal
+    notifyModal.hide()
+
+    // This variable concatenates the user's input into one string
+    var response = lowercaseChars + uppercaseChars + numericChars + specialChars;
+    var charsSet = "";
+    
+    // This function generates the character set for the password based on the user's input
+    function findCharsSet(type) {
+      var lowercase = "abcdefghijklmnopqrstuvwxyz";
+      var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var number = "0123456789";
+      var special = "!:#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+      if (type[0] === "Y") {
+        charsSet += lowercase
+      };
+      if (type[1] === "Y") {
+        charsSet += uppercase
+      };
+      if (type[2] === "Y") {
+        charsSet += number
+      };
+      if (type[3] === "Y") {
+        charsSet += special
+      };
+    }
+
+    // This calls the findCharsSet() function to generate the character set
+    findCharsSet(response);
+    var result = ""
+
+    // This loop generates the password using the generated character set and password length
+    for (var i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * charsSet.length);
+      result += charsSet.charAt(randomNumber);
+    };
+
+    // This returns the generated password
+    return result;
+  };
+
+  // This writes the generated password to the HTML document
+  passwordText.value = password;
+};
+
+// This adds an event listener to the "Generate Password" button to trigger the writePassword() function
+generateBtn.addEventListener("click", writePassword);
+
+// These variables select the necessary elements in the notification modal
+const notifyModalLabel = document.getElementById('notifyModalLabel');
+const notifyModalBody = document.getElementById('notifyModalBody');
+const notifyModalButton = document.getElementById('notifyModalButton');
+
+// This event listener disables the notification modal button when it is clicked
+notifyModalButton.addEventListener('click', () => {
+    notifyModalButton.setAttribute('disabled', true);
+});
+
+// This function shows an error message in the notification modal
+function myAlert(message) {
+  notifyModalLabel.innerText = 'Error';
+  notifyModalBody.innerText = message;
+  notifyModalButton.removeAttribute('disabled');
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// This function creates a custom prompt using the notification modal and returns the user's input
+async function myPrompt(question, type) {
+
+  // This shows the notification modal and sets the question as the label
+  notifyModal.show();
+  notifyModalLabel.innerText = question;
+  let result = null;
+  
+  // This condition generates a number input field in the notification modal body
+  if (type === 'number') {
+    notifyModalBody.innerHTML = `
+      <div class="col-md-3">
+        <input min='8' max='128' type="number" class="form-control" id="validationServer05" aria-describedby="validationServer05Feedback" required>
+        <div id="validationServer05Feedback" class="invalid-feedback">
+        </div>
+      </div>
+    `;
+
+    // This event listener updates the result variable with the input value and enables/disables the modal button based on input validity
+    notifyModalBody.querySelector('input').addEventListener('input', (event) => {
+    let value = event.target.value;
+    result = value;
+    if (value.match(/[0-9]/g) && +value >= 8 && +value <= 128) {
+      notifyModalButton.removeAttribute('disabled');
+    } else {
+      notifyModalButton.setAttribute('disabled',true);
+      }
+    });
+
+  // This condition generates a boolean input field in the notification modal body
+  } else if (type === 'boolean') {
+    notifyModalBody.innerHTML = `
+      <div class="form-check">
+        <input type="radio" class="form-check-input" id="validationFormCheck2" name="radio-stacked" required value='Y'>
+        <label class="form-check-label" for="validationFormCheck2">Yes</label>
+      </div>
+      <div class="form-check mb-3">
+        <input type="radio" class="form-check-input" id="validationFormCheck3" name="radio-stacked" required value='N'>
+        <label class="form-check-label" for="validationFormCheck3">No</label>
+        <div class="invalid-feedback">More example invalid feedback text</div>
+      </div>
+    `;
+
+    // This event listener updates the result variable with the selected radio button value and enables the modal button
+    notifyModalBody.querySelectorAll('input').forEach(input => {
+      input.addEventListener('change', (event) => {
+      result = event.target.value;
+      notifyModalButton.removeAttribute('disabled');
+      })
+    })
+  }
+
+  // This returns a promise that resolves with the user's input when the modal button is clicked
+  return new Promise((res) => {
+    notifyModalButton.addEventListener('click', () => {
+      res(result)
+    })
+  })
+}
