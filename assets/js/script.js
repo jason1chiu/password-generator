d49def177fcba3b7a3ed9df05fc9ad413f2e899d
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-03-06 01:30:36 -0500

    update prompts and alerts to modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,131 +1,137 @@
-// This variable selects the button element with the ID "generate"
+// Select the "Generate Password" button
 var generateBtn = document.querySelector("#generate");
 
-// This creates a new instance of the Bootstrap Modal for notifications
+// Create a new Bootstrap modal instance
 const notifyModal = new bootstrap.Modal(document.getElementById('notifyModal'), {})
 
-// This function writes the generated password to the password field in the HTML document
+// Define the main password generation function
 async function writePassword() {
 
-  // This variable awaits the generated password from the generatePassword() function
+  // Call the generatePassword function and wait for it to complete
   var password = await generatePassword();
 
-  // This variable selects the HTML element where the generated password will be written
+  // Select the text input where the password will be displayed
   var passwordText = document.querySelector("#password");
 
-  // This function generates the password with user inputs and returns it
+  // Define the function to generate the password
   async function generatePassword() {
-    
-    // These variables await user input from the custom prompt function myPrompt()
-    var passwordLength = await myPrompt("Choose the number of characters (8 to 128) in your password.", 'number');
-    var lowercaseChars = await myPrompt("Do you want to include lowercase characters in your password? [Y for yes or N for no]", 'boolean');
-    var uppercaseChars = await myPrompt("Do you want to include uppercase characters in your password? [Y for yes or N for no]",'boolean');
-    var numericChars = await myPrompt("Do you want to include numeric characters in your password? [Y for yes or N for no]", 'boolean');
+
+    // Prompt the user for the password length, lowercase, uppercase, numeric, and special characters
+    var passwordLength = await myPrompt("Choose the number of characters (8 to 128) in your password.",'number');
+    var lowercaseChars = await myPrompt("Do you want to include lowercase characters in your password?", 'boolean');
+    var uppercaseChars = await myPrompt("Do you want to include uppercase characters in your password?", 'boolean');
+    var numericChars = await myPrompt("Do you want to include numeric characters in your password? [Y for yes or N for no]",'boolean');
     var specialChars = await myPrompt("Do you want to include special characters in your password? [Y for yes or N for no]",'boolean');
 
-    // This condition checks if all user responses are "N" and shows an error message if they are
-    if (lowercaseChars === "N" && uppercaseChars === "N" && numericChars === "N" && specialChars === "N") {
-      myAlert("Your responses cannot all be 'No'");
+    // Combine the user's responses into a single string
+    var response = lowercaseChars + uppercaseChars + numericChars + specialChars;
+
+    // Check if the user did not select any character types
+    if (response === "NNNN") {
+      myAlert("Invalid responses to create password");
+    } else {
+      // Hide the modal
+      notifyModal.hide();
     }
 
-    // This hides the notification modal
-    notifyModal.hide()
-
-    // This variable concatenates the user's input into one string
-    var response = lowercaseChars + uppercaseChars + numericChars + specialChars;
+    // Define the character set based on the user's selections
     var charsSet = "";
-    
-    // This function generates the character set for the password based on the user's input
     function findCharsSet(type) {
       var lowercase = "abcdefghijklmnopqrstuvwxyz";
       var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       var number = "0123456789";
       var special = "!:#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-      if (type[0] === "Y") {
-        charsSet += lowercase
-      };
-      if (type[1] === "Y") {
-        charsSet += uppercase
-      };
-      if (type[2] === "Y") {
-        charsSet += number
-      };
-      if (type[3] === "Y") {
-        charsSet += special
-      };
+      if (type[0] === "Y") { charsSet += lowercase };
+      if (type[1] === "Y") { charsSet += uppercase };
+      if (type[2] === "Y") { charsSet += number };
+      if (type[3] === "Y") { charsSet += special };
     }
-
-    // This calls the findCharsSet() function to generate the character set
     findCharsSet(response);
-    var result = ""
 
-    // This loop generates the password using the generated character set and password length
+    // Generate the random password using the defined character set and password length
+    var result = ""
     for (var i = 0; i < passwordLength; i++) {
       var randomNumber = Math.floor(Math.random() * charsSet.length);
       result += charsSet.charAt(randomNumber);
-    };
+    }
 
-    // This returns the generated password
+    // Return the generated password
     return result;
-  };
+  }
 
-  // This writes the generated password to the HTML document
+  // Display the generated password in the password text input
   passwordText.value = password;
-};
+}
 
-// This adds an event listener to the "Generate Password" button to trigger the writePassword() function
+// Add event listener to the "Generate Password" button
 generateBtn.addEventListener("click", writePassword);
 
-// These variables select the necessary elements in the notification modal
+// Select the elements in the modal that will be updated
 const notifyModalLabel = document.getElementById('notifyModalLabel');
 const notifyModalBody = document.getElementById('notifyModalBody');
 const notifyModalButton = document.getElementById('notifyModalButton');
+const closeModalButton = document.getElementById('closeModalButton');
 
-// This event listener disables the notification modal button when it is clicked
+// Add event listener to the "Generate Password" button in the modal to disable it
 notifyModalButton.addEventListener('click', () => {
     notifyModalButton.setAttribute('disabled', true);
 });
 
-// This function shows an error message in the notification modal
+// Define a function to display alerts in the modal
 function myAlert(message) {
-  notifyModalLabel.innerText = 'Error';
-  notifyModalBody.innerText = message;
-  notifyModalButton.removeAttribute('disabled');
-}
-
-// This function creates a custom prompt using the notification modal and returns the user's input
-async function myPrompt(question, type) {
-
-  // This shows the notification modal and sets the question as the label
   notifyModal.show();
-  notifyModalLabel.innerText = question;
-  let result = null;
+    // Set the alert message
+    notifyModalBody.innerText = message;
+
+    // Enable the "Close" button
+    closeModalButton.removeAttribute('disabled');
   
-  // This condition generates a number input field in the notification modal body
-  if (type === 'number') {
-    notifyModalBody.innerHTML = `
+    // Add event listener to the "Close" button to hide the modal
+    closeModalButton.addEventListener('click', () => {
+      notifyModal.hide();
+    });
+  };
+  
+  // Define a function to display prompts in the modal
+  async function myPrompt(question, type) {
+  
+    // Show the modal and disable the "Generate" button
+    notifyModal.show();
+    closeModalButton.setAttribute('disabled', true);
+  
+    // Set the question as the modal label
+    notifyModalLabel.innerText = question;
+  
+    // Initialize the result variable to null
+    let result = null;
+  
+    // Check the type of prompt (number or boolean)
+    if (type === 'number') {
+  
+      // Show a number input with a minimum of 8 and a maximum of 128
+      notifyModalBody.innerHTML = `
       <div class="col-md-3">
         <input min='8' max='128' type="number" class="form-control" id="validationServer05" aria-describedby="validationServer05Feedback" required>
-        <div id="validationServer05Feedback" class="invalid-feedback">
-        </div>
+        <div id="validationServer05Feedback" class="invalid-feedback"></div>
       </div>
-    `;
-
-    // This event listener updates the result variable with the input value and enables/disables the modal button based on input validity
-    notifyModalBody.querySelector('input').addEventListener('input', (event) => {
-    let value = event.target.value;
-    result = value;
-    if (value.match(/[0-9]/g) && +value >= 8 && +value <= 128) {
-      notifyModalButton.removeAttribute('disabled');
-    } else {
-      notifyModalButton.setAttribute('disabled',true);
-      }
-    });
-
-  // This condition generates a boolean input field in the notification modal body
-  } else if (type === 'boolean') {
-    notifyModalBody.innerHTML = `
+      `;
+  
+      // Add event listener to the number input to validate the input and enable the "Generate" button
+      notifyModalBody.querySelector('input').addEventListener('input', (event) => {
+        let value = event.target.value;
+        result = value;
+        if (value.match(/[0-9]/g) && +value >= 8 && +value <= 128) {
+          notifyModalButton.removeAttribute('disabled');
+        } else {
+          notifyModalButton.setAttribute('disabled', true);
+        }
+      });
+  
+    } else if (type === 'boolean') {
+  
+      // Show two radio buttons for "Yes" and "No" options
+      notifyModalBody.innerHTML = `
       <div class="form-check">
         <input type="radio" class="form-check-input" id="validationFormCheck2" name="radio-stacked" required value='Y'>
         <label class="form-check-label" for="validationFormCheck2">Yes</label>
@@ -135,21 +141,22 @@ async function myPrompt(question, type) {
         <label class="form-check-label" for="validationFormCheck3">No</label>
         <div class="invalid-feedback">More example invalid feedback text</div>
       </div>
-    `;
-
-    // This event listener updates the result variable with the selected radio button value and enables the modal button
-    notifyModalBody.querySelectorAll('input').forEach(input => {
-      input.addEventListener('change', (event) => {
-      result = event.target.value;
-      notifyModalButton.removeAttribute('disabled');
-      })
-    })
-  }
-
-  // This returns a promise that resolves with the user's input when the modal button is clicked
-  return new Promise((res) => {
-    notifyModalButton.addEventListener('click', () => {
-      res(result)
-    })
-  })
-}
+      `;
+  
+      // Add event listener to the radio buttons to get the selected value and enable the "Generate" button
+      notifyModalBody.querySelectorAll('input').forEach(input => {
+        input.addEventListener('change', (event) => {
+          result = event.target.value;
+          notifyModalButton.removeAttribute('disabled');
+        });
+      });
+    };
+  
+    // Return a Promise that resolves to the user's response
+    return new Promise((res, rej) => {
+      notifyModalButton.addEventListener('click', () => {
+        res(result);
+      });
+    });
+  };
+  
